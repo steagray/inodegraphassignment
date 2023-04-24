594fa9958cee1f6dac514a28771e2dbb93b509f1
Steven Gray <steagray@cs2.utm.edu>
2023-04-24 13:38:01 -0500

    Completed

--- inodes.c
+++ inodes.c
@@ -5,16 +5,19 @@
 #include <string.h>
 
 // probably make the file handler for the dot graph global
-
+FILE* graph;
 
 int
-dotgraph(char* f_name, uintmax_t f_size, intmax_t a_blocks, uintmax_t i_num, uintmax_t links){
+dotgraph(char* f_name, uintmax_t f_size, intmax_t a_blocks, uintmax_t i_num, uintmax_t links, char* p_name){
 	// Add all data from this node into dot graph previously opened via main
-	printf("Name: %s\n", f_name);
-	printf("File Size: %lu\n", f_size);
-	printf("Allocated Blocks: %lu\n", a_blocks);
-	printf("I-node Number: %lu\n", i_num);
-	printf("# Of Links: %lu\n", links);
+	fprintf(graph, "\"%s\" [label=\"\\n", f_name);
+	fprintf(graph, "Name: %s\\n", f_name);
+	fprintf(graph, "File Size: %lu\\n", f_size);
+	fprintf(graph, "Allocated Blocks: %lu\\n", a_blocks);
+	fprintf(graph, "I-node Number: %lu\\n", i_num);
+	fprintf(graph, "# Of Links: %lu\\n", links);
+	fprintf(graph, "\"]\n");
+	fprintf(graph, "\"%s\" -> \"%s\"\n\n", p_name, f_name);
 	return 0;
 }
 
@@ -30,8 +33,10 @@ recur_graph(char* loc){
 	// Data Needed:
 	// - File/Directory Name
 	char* tok = strtok(loc_copy, "/");
+	char p_name[1024];
 	char f_name[1024];
 	while(tok != NULL){
+		strcpy(p_name, f_name);
 		strcpy(f_name, tok);
 		tok = strtok(NULL, "/");
 	}
@@ -41,7 +46,7 @@ recur_graph(char* loc){
 	// - i-node number
 	// - Number of links
 	// put data into dot graph
-	dotgraph(f_name, sb.st_size, sb.st_blocks, sb.st_ino, sb.st_nlink);	
+	dotgraph(f_name, sb.st_size, sb.st_blocks, sb.st_ino, sb.st_nlink, p_name);	
 
 
 	// continue with next directory
@@ -77,7 +82,11 @@ main(int argc, char* argv[]){
 		return 0;
 	}
 	//start dotgraph file here
+	graph = fopen("./graph", "w");
+	fprintf(graph, "digraph{\nrankdir=LR\n");
 	recur_graph(argv[1]);
 	//end dotgraph file after recur
+	fprintf(graph, "\n}");
+	fclose(graph);
 	return 0;
 }
